af53fe6422c537d549173b7bd16859ab26766b0b
OHJINSEONG <ojseong0828@naver.com>
2024-01-09 12:04:16 +0900

    Feat/프로그램 세팅
    프론트엔드 Home 화면띄우기

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+
+const container = document.getElementById('app');
+const root = ReactDOM.createRoot(container);
+root.render(
+    <div>
+        <App />
+    </div>
+);
